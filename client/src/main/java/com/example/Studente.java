497ac93004f807c5282fb6c7e607b068b4566b95
lai2-24 <lai2-24@itismeucci.com>
2024-01-16 14:33:30 +0100

    .

---
+++ client/src/main/java/com/example/Studente.java
@@ -0,0 +1,54 @@
+package main.java.com.example;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Studente {
+    private String nome;
+    private String cognome;
+    private Date dataDiNascita;
+
+   
+    public Studente(String nome, String cognome, Date dataDiNascita){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataDiNascita = dataDiNascita;
+    } 
+
+    
+
+    public Studente() {
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public Date getDataDiNascita() {
+        return dataDiNascita;
+    }
+
+    public void setDataDiNascita(Date dataDiNascita) {
+        this.dataDiNascita = dataDiNascita;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Nome: "+nome+" \tCognome: "+cognome+" \tNato il: "+dataDiNascita;
+    }
+}
